87441598e1142f6ec5946550338e081125023244
bizhermit <rsenda@bizhermit.com>
2022-02-04 02:05:55 +0900

    rename

--- src/cli.ts
+++ src/cli.ts
@@ -1,42 +1,59 @@
 #! /usr/bin/env node
 
-import { minify } from "./scripts/minify";
+import { minify } from ".";
 
 process.stdout.write(`::: minifier v${require("../package.json").version} :::\n`);
 
-const argv = process.argv;
-let dir: string = process.argv[2] || "./";
+const getArgV = (key: string) => {
+  const index = process.argv.findIndex(v => v === key);
+  if (index < 0) return null;
+  const val = process.argv[index + 1];
+  if (val.startsWith("-")) return null;
+  return val;
+};
+
+let dirname: string = process.argv[2] || process.cwd();
+if (dirname.startsWith("-")) dirname = process.cwd();
+
 const options = { infoLog: true, js: true, css: true, html: true };
+const ignores: Array<string> = [];
+const ignoreStr = getArgV("-I");
+if (ignoreStr) ignoreStr.split(",").forEach(v => ignores.push(v));
+if (ignores.indexOf("js") >= 0) options.js = false;
+if (ignores.indexOf("css") >= 0) options.css = false;
+if (ignores.indexOf("html") >= 0) options.html = false;
+
+const argv = process.argv;
 for (let i = 0, il = argv.length; i < il; i++) {
-    const arg = argv[i];
-    if (arg === "-ignore" || arg === "-i") {
-        for (let j = i + 1; j < il; j++) {
-            const argEx = argv[j];
-            if (argEx.indexOf("-") === 0) break;
-            switch (argEx) {
-                case "js":
-                    options.js = false;
-                    break;
-                case "css":
-                    options.css = false;
-                    break;
-                case "html":
-                    options.html = false;
-                    break;
-                default:
-                    break;
-            }
-        }
+  const arg = argv[i];
+  if (arg === "-ignore" || arg === "-i") {
+    for (let j = i + 1; j < il; j++) {
+      const argEx = argv[j];
+      if (argEx.indexOf("-") === 0) break;
+      switch (argEx) {
+        case "js":
+          options.js = false;
+          break;
+        case "css":
+          options.css = false;
+          break;
+        case "html":
+          options.html = false;
+          break;
+        default:
+          break;
+      }
     }
+  }
 }
 process.stdout.write(`${JSON.stringify(options)}\n`);
 try {
-    minify(dir, options).catch((e: any) => {
-        process.stderr.write(`${String(e)}\n`);
-    }).finally(() => {
-        process.stdout.write(`minify fin.\n`);
-    });
-} catch(e) {
+  minify(dirname, options).catch((e: any) => {
     process.stderr.write(`${String(e)}\n`);
-    process.stdout.write(`minify failed fin.\n`);
+  }).finally(() => {
+    process.stdout.write(`minify fin.\n`);
+  });
+} catch (err) {
+  process.stderr.write(`${String(err)}\n`);
+  process.stdout.write(`minify failed fin.\n`);
 }
---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import fs from "fs";
+import path from "path";
+import terser from "terser";
+import csso from "csso";
+import htmlMinifier from "html-minifier";
+
+type Options = {
+  infoLog?: boolean;
+  js?: boolean;
+  css?: boolean;
+  html?: boolean;
+};
+
+export const minify = async (srcDir: string, options?: Options) => {
+  if (srcDir == null || srcDir === "") throw new Error("not set source directory");
+
+  const infoLog = options?.infoLog === true;
+  const js = options?.js !== false;
+  const css = options?.css !== false;
+  const html = options?.html !== false;
+
+  const impl = async (dir: string) => {
+    const names = fs.readdirSync(dir);
+    for await (const name of names) {
+      if (name.startsWith(".")) continue;
+      const fullName = path.join(dir, name);
+      const stats = fs.statSync(fullName);
+      if (stats.isDirectory()) {
+        await impl(fullName);
+        continue;
+      }
+      try {
+        if (/.*\.js$/.test(name)) {
+          if (!js) continue;
+          if (infoLog) process.stdout.write(`js   : ${fullName}\n`)
+          fs.writeFileSync(fullName, await minifyJs(extractData(fullName)));
+          continue;
+        }
+        if (/.*\.css$/.test(name)) {
+          if (!css) continue;
+          if (infoLog) process.stdout.write(`css  : ${fullName}\n`);
+          fs.writeFileSync(fullName, await minifyCss(extractData(fullName)));
+          continue;
+        }
+        if (/.*\.html$/.test(name)) {
+          if (!html) continue;
+          if (infoLog) process.stdout.write(`html : ${fullName}\n`);
+          fs.writeFileSync(fullName, await minifyHtml(extractData(fullName)));
+          continue;
+        }
+      } catch (err) {
+        process.stderr.write(`${String(err)}\n`);
+      }
+    }
+  };
+  await impl(srcDir);
+};
+
+const extractData = (fullName: string) => {
+  return fs.readFileSync(fullName, "utf8").toString();
+};
+
+export const minifyJs = async (content: string) => {
+  const ret = await terser.minify(content);
+  return ret.code;
+};
+
+export const minifyCss = async (content: string) => {
+  return csso.minify(content).css;
+};
+
+export const minifyHtml = (content: string) => {
+  return new Promise<string>((resolve) => {
+    const ret = htmlMinifier.minify(content, {
+      caseSensitive: true,
+      collapseWhitespace: true,
+    });
+    resolve(ret);
+  });
+};
