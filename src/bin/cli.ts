113f925892db120f781ff2b77b07cb91289f1649
bizhermit <rsenda@bizhermit.com>
2022-07-31 02:35:12 +0900

    organize

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,13 +1,18 @@
 #! /usr/bin/env node
 
+import path from "path";
 import { getArg, getKeyArg, hasKeyArg } from "@bizhermit/cli-sdk";
 import minifier from "../dist";
 
 const quiet = hasKeyArg("--quiet");
 if (!quiet) process.stdout.write(`::: minifier v${require("../package.json").version} :::\n`);
 
-let dirname: string = getArg() || process.cwd();
-if (dirname.startsWith("-")) dirname = process.cwd();
+let cwd = process.cwd();
+const argPath = getArg();
+if (argPath && !argPath.startsWith("-")) {
+  if (argPath[1] === ":" || argPath.startsWith("\\\\")) cwd = argPath;
+  else cwd = path.join(cwd, argPath);
+}
 
 const options = { infoLog: !quiet, js: true, css: true, html: true };
 const ignores: Array<string> = [];
@@ -19,12 +24,12 @@ if (ignores.indexOf("html") >= 0) options.html = false;
 
 if (!quiet) process.stdout.write(`${JSON.stringify(options)}\n`);
 try {
-    minifier.minify(dirname, options).catch((e: any) => {
-        process.stderr.write(`${String(e)}\n`);
-    }).finally(() => {
-        if (!quiet) process.stdout.write(`minify fin.\n`);
-    });
+  minifier.minify(cwd, options).catch((e: any) => {
+    process.stderr.write(`${String(e)}\n`);
+  }).finally(() => {
+    if (!quiet) process.stdout.write(`minify fin.\n`);
+  });
 } catch (err) {
-    process.stderr.write(`${String(err)}\n`);
-    if (!quiet) process.stdout.write(`minify failed fin.\n`);
+  process.stderr.write(`${String(err)}\n`);
+  if (!quiet) process.stdout.write(`minify failed fin.\n`);
 }
